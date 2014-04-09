68c0f9952415b3e90cd2cc1766c04a7eb1ff2156
Liang Liang <liangbright@gmail.com>
2014-04-09 17:33:58 -0400

    update

--- Source/LinearAlgebra/DenseMatrix/mdkLinearAlgebra_Function_DenseMatrix_Part_3.h
+++ Source/LinearAlgebra/DenseMatrix/mdkLinearAlgebra_Function_DenseMatrix_Part_3.h
@@ -22,8 +22,20 @@ template<typename ElementType>
 DenseMatrix<ElementType> ComputeL2DistanceListFromSingleVectorToColVectorSet(const DenseMatrix<ElementType>& SingleVector,
                                                                              const DenseMatrix<ElementType>& ColVectorSet);
 
+template<typename ElementType>
+DenseMatrix<ElementType> ComputeL1DistanceListFromSingleVectorToColVectorSet(const DenseMatrix<ElementType>& SingleVector,
+                                                                             const DenseMatrix<ElementType>& ColVectorSet);
 
+// sum((x-mean(x)).*(y-mean(y))/(sum((x-mean(x)).^2)*sum((y-mean(y)).^2))
+template<typename ElementType>
+DenseMatrix<ElementType> ComputeCorrelationListFromSingleVectorToColVectorSet(const DenseMatrix<ElementType>& SingleVector,
+                                                                              const DenseMatrix<ElementType>& ColVectorSet);
 
+// sum(x.*y)/(sum(x.^2)*sum(y.^2))
+// UncenteredCorrelation = angle between two vectors
+template<typename ElementType>
+DenseMatrix<ElementType> ComputeUncenteredCorrelationListFromSingleVectorToColVectorSet(const DenseMatrix<ElementType>& SingleVector,
+                                                                                        const DenseMatrix<ElementType>& ColVectorSet);
 }
 
 #include "mdkLinearAlgebra_Function_DenseMatrix_Part_3.hpp"
--- Source/LinearAlgebra/DenseMatrix/mdkLinearAlgebra_Function_DenseMatrix_Part_3.hpp
+++ Source/LinearAlgebra/DenseMatrix/mdkLinearAlgebra_Function_DenseMatrix_Part_3.hpp
@@ -41,13 +41,13 @@ DenseMatrix<ElementType> ComputeL2DistanceListFromSingleVectorToColVectorSet(con
 
     for (int_max j = 0; j < Size.ColNumber; ++j)
     {
-        auto BeginPointer_j = ElementPointer + j*Size.RowNumber;
+        auto Vector_j = ElementPointer + j*Size.RowNumber;
 
         ElementType Distance_j = ElementType(0);
 
         for (int_max i = 0; i < Size.RowNumber; ++i)
         {
-            auto temp = SingleVector[i] - BeginPointer_j[i];
+            auto temp = SingleVector[i] - Vector_j[i];
             Distance_j += temp*temp;
         }
 
@@ -56,6 +56,171 @@ DenseMatrix<ElementType> ComputeL2DistanceListFromSingleVectorToColVectorSet(con
 
     return L2DistanceList;
 }
+
+
+template<typename ElementType>
+DenseMatrix<ElementType> ComputeL1DistanceListFromSingleVectorToColVectorSet(const DenseMatrix<ElementType>& SingleVector,
+    const DenseMatrix<ElementType>& ColVectorSet)
+{
+    DenseMatrix<ElementType> L1DistanceList;
+
+    auto Size = ColVectorSet.GetSize();
+
+    if (Size.ColNumber == 0)
+    {
+        MDK_Error("Empty Dictionary @ mdkLinearAlgebra_DenseMatrix ComputeL1DistanceListFromSingleVectorToColVectorSet(...)")
+            return L1DistanceList;
+    }
+
+    if (SingleVector.IsVector() == false)
+    {
+        MDK_Error("Input vector is not a vector @ mdkLinearAlgebra_DenseMatrix ComputeL1DistanceListFromSingleVectorToColVectorSet(...)")
+        return L1DistanceList;
+    }
+
+    if (SingleVector.GetElementNumber() != Size.RowNumber)
+    {
+        MDK_Error("Size does not match @ mdkLinearAlgebra_DenseMatrix ComputeL1DistanceListFromSingleVectorToColVectorSet(...)")
+        return L1DistanceList;
+    }
+
+    L1DistanceList.FastResize(1, Size.RowNumber);
+
+    auto ElementPointer = ColVectorSet.GetElementPointer();
+
+    for (int_max j = 0; j < Size.ColNumber; ++j)
+    {
+        auto Vector_j = ElementPointer + j*Size.RowNumber;
+
+        ElementType Distance_j = ElementType(0);
+
+        for (int_max i = 0; i < Size.RowNumber; ++i)
+        {
+            Distance_j = std::abs(SingleVector[i] - Vector_j[i]);
+        }
+
+        L1DistanceList[j] = Distance_j;
+    }
+
+    return L1DistanceList;
+}
+
+
+template<typename ElementType>
+DenseMatrix<ElementType> ComputeCorrelationListFromSingleVectorToColVectorSet(const DenseMatrix<ElementType>& SingleVector,
+                                                                              const DenseMatrix<ElementType>& ColVectorSet)
+{
+    DenseMatrix<ElementType> CorrelationList;
+
+    auto Size = ColVectorSet.GetSize();
+
+    if (Size.ColNumber == 0)
+    {
+        MDK_Error("Empty Dictionary @ mdkLinearAlgebra_DenseMatrix ComputeCorrelationListFromSingleVectorToColVectorSet(...)")
+        return CorrelationList;
+    }
+
+    if (SingleVector.IsVector() == false)
+    {
+        MDK_Error("Input vector is not a vector @ mdkLinearAlgebra_DenseMatrix ComputeCorrelationListFromSingleVectorToColVectorSet(...)")
+        return CorrelationList;
+    }
+
+    if (SingleVector.GetElementNumber() != Size.RowNumber)
+    {
+        MDK_Error("Size does not match @ mdkLinearAlgebra_DenseMatrix ComputeCorrelationListFromSingleVectorToColVectorSet(...)")
+        return CorrelationList;
+    }
+
+    CorrelationList.FastResize(1, Size.RowNumber);
+
+    auto ElementPointer = ColVectorSet.GetElementPointer();
+
+    for (int_max j = 0; j < Size.ColNumber; ++j)
+    {
+        auto Vector_j = ElementPointer + j*Size.RowNumber;
+
+        ElementType Mean_s = ElementType(0);
+
+        ElementType Mean_j = ElementType(0);
+
+        for (int_max i = 0; i < Size.RowNumber; ++i)
+        {
+            Mean_s += SingleVector[i];
+            Mean_j += Vector_j[i];
+        }
+        Mean_s /= ElementType(Size.RowNumber);
+        Mean_j /= ElementType(Size.RowNumber);
+
+        ElementType part_a = ElementType(0);
+        ElementType part_b = ElementType(0);
+        ElementType part_c = ElementType(0);
+
+        for (int_max i = 0; i < Size.RowNumber; ++i)
+        {
+            part_a += (SingleVector[i] - Mean_s)*(Vector_j[i] - Mean_j);
+            part_b += (SingleVector[i] - Mean_s)*(SingleVector[i] - Mean_s);
+            part_c += (Vector_j[i] - Mean_j)*(Vector_j[i] - Mean_j);
+        }
+
+        CorrelationList[j] = part_a / (std::sqrt(part_b) * std::sqrt(part_c));
+    }
+
+    return CorrelationList;
+}
+
+
+template<typename ElementType>
+DenseMatrix<ElementType> ComputeUncenteredCorrelationListFromSingleVectorToColVectorSet(const DenseMatrix<ElementType>& SingleVector,
+    const DenseMatrix<ElementType>& ColVectorSet)
+{
+    DenseMatrix<ElementType> CorrelationList;
+
+    auto Size = ColVectorSet.GetSize();
+
+    if (Size.ColNumber == 0)
+    {
+        MDK_Error("Empty Dictionary @ mdkLinearAlgebra_DenseMatrix ComputeUncenteredCorrelationListFromSingleVectorToColVectorSet(...)")
+            return CorrelationList;
+    }
+
+    if (SingleVector.IsVector() == false)
+    {
+        MDK_Error("Input vector is not a vector @ mdkLinearAlgebra_DenseMatrix ComputeUncenteredCorrelationListFromSingleVectorToColVectorSet(...)")
+            return CorrelationList;
+    }
+
+    if (SingleVector.GetElementNumber() != Size.RowNumber)
+    {
+        MDK_Error("Size does not match @ mdkLinearAlgebra_DenseMatrix ComputeUncenteredCorrelationListFromSingleVectorToColVectorSet(...)")
+            return CorrelationList;
+    }
+
+    CorrelationList.FastResize(1, Size.RowNumber);
+
+    auto ElementPointer = ColVectorSet.GetElementPointer();
+
+    for (int_max j = 0; j < Size.ColNumber; ++j)
+    {
+        auto Vector_j = ElementPointer + j*Size.RowNumber;
+
+        ElementType part_a = ElementType(0);
+        ElementType part_b = ElementType(0);
+        ElementType part_c = ElementType(0);
+
+        for (int_max i = 0; i < Size.RowNumber; ++i)
+        {
+            part_a += SingleVector[i] * Vector_j[i];
+            part_b += SingleVector[i] * SingleVector[i];
+            part_c += Vector_j[i] * Vector_j[i];
+        }
+
+        CorrelationList[j] = part_a / (std::sqrt(part_b) * std::sqrt(part_c));
+    }
+
+    return CorrelationList;
+}
+
 
 }
 
