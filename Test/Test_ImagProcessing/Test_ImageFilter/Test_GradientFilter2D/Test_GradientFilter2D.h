b535124dd59df9d5d9a887ad26f12abb8339737c
liangbright <liangbright@gmail.com>
2016-06-06 23:12:48 -0400

    update

--- Test/Test_ImagProcessing/Test_ImageFilter/Test_GradientFilter2D/Test_GradientFilter2D.h
+++ Test/Test_ImagProcessing/Test_ImageFilter/Test_GradientFilter2D/Test_GradientFilter2D.h
@@ -8,6 +8,7 @@
 
 #include "mdkDenseImage2D_FileIO.h"
 #include "mdkGradientDenseImageFilter2D.h"
+#include "mdkDiscreteConvolutionDenseImageFilter2D.h"
 
 namespace mdk
 {
@@ -53,6 +54,73 @@ void test_a()
 	//DenseVector<double, 2> GradientPrior = { 1.0, 0.0 };
 	//GFilter.SetGradientDirectionPrior(GradientPrior);
 
+	GFilter.Update();
+	const auto& GradientImage = GFilter.OutputImage();
+
+	auto t1 = std::chrono::system_clock::now();
+	std::chrono::duration<double> raw_time = t1 - t0;
+	std::cout << "time " << raw_time.count() << '\n';
+
+	std::cout << "done" << '\n';
+
+	DenseImage2D<double> GradientMagImage;
+	GradientMagImage.SetInfo(GradientImage.GetInfo());
+	for (int_max k = 0; k < GradientMagImage.GetPixelCount(); ++k)
+	{
+		GradientMagImage[k] = GradientImage[k].L2Norm();
+	}
+
+	Save2DScalarImageAsJsonDataFile(GradientMagImage, FileNameAndPath_OutputImage);
+}
+
+
+void test_b()
+{
+	String FilePath_InputImage = "C:/Research/LumbarSpine/TestData/Patient1/T2W/T2W0005.dcm";
+
+	String Test_Path = "C:/Research/MDK/MDK_Build/Test/Test_ImageProcessing/Test_ImageFilter/Test_GradientFilter2D/TestData/";
+
+	String FileNameAndPath_OutputImage = Test_Path + "test_b_GradientMagImage.json";
+
+	DenseImage2D<double> InputImage;
+	Load2DScalarImageFromSingleDICOMFile(InputImage, FilePath_InputImage);
+	//auto InputImage = Load3DScalarImageFromJsonDataFile<double>(FilePath_InputImage);
+
+	std::cout << "start" << '\n';
+	auto t0 = std::chrono::system_clock::now();
+
+	DiscreteConvolutionDenseImageFilter2D<double> imfilter;
+	imfilter.SetInputImage(&InputImage);
+	imfilter.SetBoundaryOptionAsReplicate();
+	imfilter.CreateGaussianMask(InputImage.GetSpacing(), 1, 1, 1.5);
+	imfilter.SetMaxThreadCount(8);
+	imfilter.Update();
+
+	GradientDenseImageFilter2D<double> GFilter;
+	GFilter.SetInputImage(&imfilter.OutputImage());
+
+	auto OutputImageInfo = InputImage.GetInfo();
+	//OutputImageInfo.Spacing *= 2.0;
+	//OutputImageInfo.Size = {256, 256};
+	GFilter.SetOutputImageInfo(OutputImageInfo);
+
+	//Resampler.SetOutputImageInfoBySize(256, 256);
+
+	auto InterpolationOption = GFilter.GetImageInterpolationOption();
+	InterpolationOption.MethodType = GradientDenseImageFilter2D<double>::ImageInterpolationMethodEnum::Linear;
+	InterpolationOption.BoundaryOption = GradientDenseImageFilter2D<double>::ImageInterpolationBoundaryOptionEnum::Constant;
+	InterpolationOption.Pixel_OutsideImage = 0;
+	GFilter.SetImageInterpolationOption(InterpolationOption);
+	
+	GFilter.SetRadius(1.0);
+	GFilter.SetAngleResolution(0.0175);
+
+	//GFilter.BuildMask();
+
+	//DenseVector<double, 2> GradientPrior = { 1.0, 0.0 };
+	//GFilter.SetGradientDirectionPrior(GradientPrior);
+
+	GFilter.SetMaxThreadCount(8);
 	GFilter.Update();
 	const auto& GradientImage = GFilter.OutputImage();
 
--- Test/Test_ImagProcessing/Test_ImageFilter/Test_GradientFilter2D/Test_GradientFilter2D.m
+++ Test/Test_ImagProcessing/Test_ImageFilter/Test_GradientFilter2D/Test_GradientFilter2D.m
@@ -1,5 +1,12 @@
+I=dicomread('C:/Research/LumbarSpine/TestData/Patient1/T2W/T2W0005.dcm');
+imtool(I)
+%%
 [ImageA, ImageInfoA]=ReadDenseImage2DFromJsonDataFile('C:/Research/MDK/MDK_Build/Test/Test_ImageProcessing/Test_ImageFilter/Test_GradientFilter2D/TestData/GradientMagImage.json');
 %%
 imtool(ImageA')
 %%
 figure; hist(ImageA(:), 100)
+%%
+[ImageB, ImageInfoB]=ReadDenseImage2DFromJsonDataFile('C:/Research/MDK/MDK_Build/Test/Test_ImageProcessing/Test_ImageFilter/Test_GradientFilter2D/TestData/test_b_GradientMagImage.json');
+%%
+imtool(ImageB')
