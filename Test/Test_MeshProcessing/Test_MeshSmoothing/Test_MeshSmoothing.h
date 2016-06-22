4f794c29b96c4be3d83e25a75f28909d1eb4261f
liangbright <liangbright@gmail.com>
2016-06-22 00:19:52 -0400

    update

--- Test/Test_MeshProcessing/Test_MeshSmoothing/Test_MeshSmoothing.h
+++ Test/Test_MeshProcessing/Test_MeshSmoothing/Test_MeshSmoothing.h
@@ -8,13 +8,15 @@ void Test_Aorta()
 {
 	std::string FilePathAndName = "C:/Research/MDK/MDK_Build/Test/Test_MeshProcessing/Test_MeshSmoothing/TestData/Aorta";
 
-	PolygonMesh<PolygonMeshStandardAttributeType<double>> InputMesh;
-	LoadPolygonMeshFromVTKFile(InputMesh, FilePathAndName + ".vtk");
+	TriangleMesh<PolygonMeshStandardAttributeType<double>> InputMesh;
+	LoadTriangleMeshFromVTKFile(InputMesh, FilePathAndName + ".vtk");
 
 	auto OutputMesh1 = SmoothMeshByVTKSmoothPolyDataFilter(InputMesh, 100, false);
 	auto OutputMesh2 = SmoothMeshByVTKWindowedSincPolyDataFilter(InputMesh, 0.01, 10);
-
+	auto OutputMesh3 = InputMesh;
+	SmoothTriangleMeshByNormalBasedCurvature(OutputMesh3, 10, 1, false, false);
 	SavePolygonMeshAsVTKFile(OutputMesh1, FilePathAndName + "_VTKSmoothPolyDataFilter.vtk");
-	SavePolygonMeshAsVTKFile(OutputMesh1, FilePathAndName + "_VTKWindowedSincPolyDataFilter.vtk");
+	SavePolygonMeshAsVTKFile(OutputMesh2, FilePathAndName + "_VTKWindowedSincPolyDataFilter.vtk");
+	SavePolygonMeshAsVTKFile(OutputMesh3, FilePathAndName + "_SmoothTriangleMeshByNormalBasedCurvature.vtk");
 }
 
