79b41a3141d1fa18bc8739e9df986f0272570121
liangbright <liangbright@gmail.com>
2017-07-04 21:13:24 -0400

    update

--- Source/DataStructureConversion/mdkVTKDataStructureConversion.h
+++ Source/DataStructureConversion/mdkVTKDataStructureConversion.h
@@ -74,50 +74,44 @@ inline VTKDataTypeEnum TranslateMDKScalarTypeToVTKScalarType(const String& MDKSc
 template<typename ScalarType>
 VTKDataTypeEnum GetVTKScalarType(const ScalarType& ReferenceScalar);
 
-//-------------------------------------- convert mdk Curve/Matrix to vtkPolyData ----------------------------------------------------------//
+//-------------------------------------- convert between mdk Curve and vtkPolyData ----------------------------------------------------------//
 template<typename ScalarType>
 vtkSmartPointer<vtkPolyData> ConvertSingleMDK3DCurveToVTKPolyData(const DenseMatrix<ScalarType>& MDK3DCurve);
 
 template<typename ScalarType>
 vtkSmartPointer<vtkPolyData> ConvertMultipleMDK3DCurveToVTKPolyData(const ObjectArray<DenseMatrix<ScalarType>>& MDK3DCurveList);
 
-//-------------------------------------- convert vtkPolyData to mdk Curve/Matrix ----------------------------------------------------------//
 template<typename ScalarType>
-bool ConvertVTKPolyDataToMultipleMDK3DCurve(vtkPolyData* VTKCurveData, ObjectArray<DenseMatrix<ScalarType>>& MDK3DCurveData);//multiple curve
+bool ConvertVTKPolyDataToMultipleMDK3DCurve(vtkPolyData* VTKCurveData, ObjectArray<DenseMatrix<ScalarType>>& MDK3DCurveData);
 
 template<typename ScalarType>
-bool ConvertVTKPolyDataToSingleMDK3DCurve(vtkPolyData* VTKCurveData, DenseMatrix<ScalarType>& MDK3DCurveData);//single curve
+bool ConvertVTKPolyDataToSingleMDK3DCurve(vtkPolyData* VTKCurveData, DenseMatrix<ScalarType>& MDK3DCurveData);
 
-//-------------------------------------- convert mdk 3D image to vtk 3D image (copy data) -----------------------------------------------------//
+//-------------------------------------- convert between mdk 3D image and vtk 3D image (copy data) -----------------------------------------------------//
 template<typename PixelType>
 vtkSmartPointer<vtkImageData> ConvertMDK3DScalarImageToVTK3DScalarImage(const DenseImage3D<PixelType>& MDKImage);
 
-//-------------------------------------- convert vtk 3D image to mdk 3D image (copy data) -----------------------------------------------------//
-// vtkImageData is not template, so do not share data
 template<typename PixelType>
 bool ConvertVTK3DScalarImageToMDK3DScalarImage(const vtkImageData* VTKImage, DenseImage3D<PixelType>& MDKImage);
 
-//--------------------------------------- convert mdk PolygonMesh to vtkPolyData -----------------------------------------------//
+//--------------------------------------- convert between mdk PolygonMesh and vtkPolyData -----------------------------------------------//
 template<typename ScalarType>
 vtkSmartPointer<vtkPolyData> ConvertMDKPolygonMeshToVTKPolyData(const PolygonMesh<ScalarType>& MDKMesh);
 
-//--------------------------------------- convert vtkPolyData to mdk PolygonMesh ------------------------------------------------//
 template<typename ScalarType>
 bool ConvertVTKPolyDataToMDKPolygonMesh(vtkPolyData* VTKMesh, PolygonMesh<ScalarType>& MDKMesh);
 
-//--------------------------------------- convert mdk PolyhedronMesh to vtkUnstructuredGrid ------------------------------------------------//
+//--------------------------------------- convert between mdk PolyhedronMesh and vtkUnstructuredGrid ------------------------------------------------//
 template<typename ScalarType>
 vtkSmartPointer<vtkUnstructuredGrid> ConvertMDKPolyhedronMeshToVTKUnstructuredGrid(const PolyhedronMesh<ScalarType>& MDKMesh);
 
-//--------------------------------------- convert vtkUnstructuredGrid to mdk PolyhedronMesh ------------------------------------------------//
 template<typename ScalarType>
 bool ConvertVTKUnstructuredGridToMDPolyhedronMesh(vtkUnstructuredGrid* VTKMesh, PolyhedronMesh<ScalarType>& MDKMesh);
 
-//--------------------------------------- convert mdk Mesh to vtkUnstructuredGrid ------------------------------------------------//
+//--------------------------------------- convert between mdk Mesh and vtkUnstructuredGrid ------------------------------------------------//
 template<typename ScalarType>
 vtkSmartPointer<vtkUnstructuredGrid> ConvertMDKMeshToVTKUnstructuredGrid(const Mesh<ScalarType>& MDKMesh);
 
-//--------------------------------------- convert vtkUnstructuredGrid to mdk Mesh ------------------------------------------------//
 template<typename ScalarType>
 bool ConvertVTKUnstructuredGridToMDKMesh(vtkUnstructuredGrid* VTKMesh, Mesh<ScalarType>& MDKMesh);
 
